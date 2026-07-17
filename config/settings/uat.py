d735236b20d60042a13e1a36d642761cb1228873
joselopez-moj <jose.lopez@justice.gov.uk>
2026-07-17 10:46:04 +0100

    fix: add collecstatic and whitenoise + TODO comments

--- config/settings/uat.py
+++ config/settings/uat.py
@@ -1,6 +1,11 @@
 import os
 
 from .base import *  # noqa: F403
+from .base import MIDDLEWARE as BASE_MIDDLEWARE
+
+# TEMPORARY: UAT-specific settings using SQLite until a dedicated DB instance is provisioned
+# TODO: Once UAT DB is ready, replace this file with correct pattern
+# and configure DATABASE_* env vars in helm_deploy/laa-cla-admin-service/values/values-uat.yaml
 
 DEBUG = True
 
@@ -11,6 +16,12 @@ SECRET_KEY = os.getenv(
 
 ALLOWED_HOSTS = ["*"]
 
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+] + BASE_MIDDLEWARE[1:]
+
+# TEMPORARY: SQLite database - will be replaced with PostgreSQL
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
