fb53139fd88a10fcc1f184fa98e881a2cc426b6c
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-07 16:19:21 +0100

    DEBUG - Add view to debug missing proto headers

--- apps/cla_auth/views.py
+++ apps/cla_auth/views.py
@@ -1,1 +1,28 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.views import View
+
+
+class DebugView(View):
+    def dispatch(self, request, *args, **kwargs):
+        headers = ["request headers"]
+        print("=== HEADERS ===")
+
+        for key, value in request.headers.items():
+            print(f"{key}: {value}")
+            headers.append(f"{key}: {value}")
+
+        print("=== META ===")
+        headers.append("request META")
+
+        for key, value in request.META.items():
+            if key.startswith("HTTP_") or key in (
+                "REMOTE_ADDR",
+                "SERVER_PORT",
+                "SERVER_NAME",
+                "wsgi.url_scheme",
+            ):
+                headers.append(f"{key}: {value}")
+
+                print(f"{key}: {value}")
+
+        return HttpResponse("\n".join(headers))
--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from apps.cla_auth.views import DebugView
 from config.views import status
 
 admin.site.site_header = "Civil Legal Advice Administration"
@@ -28,6 +29,7 @@ admin.site.index_title = "Manage CLA services"
 # `/admin` path should always be at the lowest priority to avoid conflicts with other apps that may use the `/admin` path.
 urlpatterns = [
     path("admin/reports/", include("apps.reports.urls")),
+    path("cla-auth/", DebugView.as_view(), name="cla-auth"),
     path("admin/", admin.site.urls),
     path("status", status, name="status"),
     path("oauth2/", include("django_entra_auth.urls")),
