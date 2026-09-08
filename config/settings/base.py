f5961db7d421147acff3a3b9d5dcf1d8f49a3d22
said-moj <45761276+said-moj@users.noreply.github.com>
2026-09-08 14:28:45 +0100

    Do not generate staticfiles inside the docker image

--- config/settings/base.py
+++ config/settings/base.py
@@ -128,8 +128,5 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
-STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles",
-]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
